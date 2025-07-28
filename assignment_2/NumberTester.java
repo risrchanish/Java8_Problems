77ec32da31b1f1fa4d282fdd28085b0fd49058f8
risrchanish <risrchanish@gmail.com>
2025-07-28 16:00:16 +0530

    another methods

--- assignment_2/NumberTester.java
+++ assignment_2/NumberTester.java
@@ -3,9 +3,38 @@ package assignment_2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class NumberTester {
+	
+	/* Write Below methods:
+	 * 1 `printNumbers`: Accepts a list of integers and a `NumberProcessor` 
+	 * instance and processes each number in the list using the `process` method of the `NumberProcessor`.
+	 * 
+   * `2. filterAndPrint`: Accepts a list of integers, a `NumberProcessor` instance, and a predicate function. 
+   * 	It filters the numbers in the list based on the predicate and then 
+   * 		processes each filtered number using the `NumberProcessor`.
+   * `3. sumNumbers`: Accepts a list of integers and returns the sum of all the numbers in the list.
+	 */
+	
+	public static void printNumbers(List<Integer> intList, NumberProcessor nb)
+	{
+		intList.stream().forEach(numbers -> nb.process(numbers));
+	}
+	
+	public static void filterAndPrint(List<Integer> intList, NumberProcessor nb, Predicate<Integer> pred)
+	{
+		intList.stream().filter(pred).forEach(number -> nb.process(number));
+	}
+	
+	public static int sumNumbers(List<Integer> intList)
+	{
+		BinaryOperator<Integer> sum = (int1,int2) -> int1+int2;
+		int totalSum = intList.stream().reduce(0, sum);
+		return totalSum;
+	}
 
 	public static void main(String[] args) {
 		
@@ -29,6 +58,24 @@ public class NumberTester {
 		BinaryOperator<Integer> sum = (int1,int2) -> int1+int2;
 		int totalSum = intList.stream().reduce(0, sum);
 		System.out.println("Total Sum: "+totalSum);
+		
+		
+		/*
+		 * 		 Printing all the numbers in the list.
+		 *		 Filtering and printing only the even numbers in the list.
+		 * 		Calculating and printing the sum of all the numbers in the list.
+	
+		 */
+		
+		System.out.println("========================");
+		printNumbers(intList, number -> System.out.print(number+" "));
+		
+		System.out.println("\n==========================");
+		Predicate<Integer> filter = number -> number%2 == 0;
+		filterAndPrint(intList,num -> System.out.print(num+" "), filter);
+		
+		System.out.println("\n===================");
+		System.out.println(sumNumbers(intList));
 	}
 
 }
