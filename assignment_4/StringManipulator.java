9d52f2fb78bb353dcbc52777fa5620e1fbeb33ac
risrchanish <risrchanish@gmail.com>
2025-08-03 06:15:47 +0530

    Method reference for Strings comparision

---
+++ assignment_4/StringManipulator.java
@@ -0,0 +1,15 @@
+package assignment_4;
+
+public class StringManipulator {
+
+	public static void print(String str)
+	{
+		System.out.println(str);
+	}
+	
+	
+	public static int compareLength(String str1, String str2)
+	{
+		return str1.length() - str2.length();
+	}
+}
